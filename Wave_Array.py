a62b8d4985a08b1d5647175e6dfb0b70d8d9929f
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-03-22 20:07:45 +0530

    WaveArray

---
+++ Wave_Array.py
@@ -0,0 +1,10 @@
+#https://www.interviewbit.com/problems/wave-array/
+
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def wave(self, A):
+        A.sort()
+        for i in range(0,len(A)-1,2):
+            A[i],A[i+1] = A[i+1],A[i]
+        return A
